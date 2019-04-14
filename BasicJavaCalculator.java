90750a78e5c64783e01e8238ba79dc3dbd4c9c08
NicholasS13 <37513914+NicholasS13@users.noreply.github.com>
2019-04-14 13:49:29 -0400

    added working exponent method

--- BasicJavaCalculator.java
+++ BasicJavaCalculator.java
@@ -5,7 +5,7 @@ public class BasicJavaCalculator{
 		Scanner numScan = new Scanner(System.in);
 		System.out.print("Please type in your first number: ");
 		double firstnum = numScan.nextDouble();
-		System.out.print("Please type in the opperatior you would like to use(1=+,2=-,3=/,4=*: ");
+		System.out.print("Please type in the opperatior you would like to use(1=+,2=-,3=/,4=*, 5=^: ");
 		int operator = numScan.nextInt();
 		System.out.print("Please type in your second number: ");
 		double secondnum = numScan.nextDouble();
@@ -17,18 +17,30 @@ public class BasicJavaCalculator{
 			division(firstnum,secondnum);
 		if(operator==4)
 			multiply(firstnum,secondnum);
-		System.out.println(firstnum+"+"+secondnum+"="+ans);
+		if(operator==5)
+			exponent(firstnum,secondnum);
 	}
 	public static void addition(double a,double b){
 		ans += a+b;
+		System.out.println(a+"+"+b+"="+ans);
 	}
 	public static void subtraction(double a,double b){
 		ans += a-b;
+		System.out.println(a+"-"+b+"="+ans);
 	}
 	public static void division(double a, double b){
 		ans += a/b;
+		System.out.println(a+"/"+b+"="+ans);
 	}
 	public static void multiply(double a, double b){
 		ans += a*b;
+		System.out.println(a+"x"+b+"="+ans);
+	}
+	public static void exponent(double a, double b){
+		ans = 1;
+		for(int i = 0;i<b;i++){
+			ans*=a;
+		}
+		System.out.println(a+"^"+b+"="+ans);
 	}
 }
